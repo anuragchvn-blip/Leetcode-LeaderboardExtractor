1e9e49eef7cb1e652ea2e65d4663fe5284fbebe0
Anurag chavan <118217089+anuragchvn-blip@users.noreply.github.com>
2025-02-16 15:22:52 +0530

    Update code.js

--- code.js
+++ code.js
@@ -1,20 +1,20 @@
-var leaderboardList = document.querySelectorAll('.leaderboard-list-view');
-var result = [];
+const leaderboardList = document.querySelectorAll('.leaderboard-list-view');
+const result = [];
 
-leaderboardList.forEach(function(entry) {
-    var user_info = {};
+leaderboardList.forEach(entry => {
+    const user_info = {};
 
-    var rank = entry.querySelector('.span-flex-2.text-center p').innerText.trim();
+    const rank = entry.querySelector('.span-flex-2.text-center p').innerText.trim();
     user_info['Rank'] = rank;
 
-    var name = entry.querySelector('.span-flex-4 a.leaderboard-hackername').innerText.trim();
+    const name = entry.querySelector('.span-flex-4 a.leaderboard-hackername').innerText.trim();
     user_info['Name'] = name;
 
-    var score = entry.querySelector('.span-flex-3 p').innerText.trim();
+    const score = entry.querySelector('.span-flex-3 p').innerText.trim();
     user_info['Score'] = score;
 
     result.push(user_info);
 });
 
-var json_result = JSON.stringify(result, null, 2);
+const json_result = JSON.stringify(result, null, 2);
 console.log(json_result);
